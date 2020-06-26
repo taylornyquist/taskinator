91f29c11d3fd66284efa4183bb6986183fad1a94
Taylor Nyquist <tnyquist@gmail.com>
2020-06-26 02:26:37 -0500

    finished drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,6 +64,7 @@ var createTaskEl = function (taskDataObj) {
 
     // add task id as a custom attriute (and set value to taskIdCounter (which starts at 0))
     listItemEl.setAttribute("data-task-id", taskIdCounter);
+    listItemEl.setAttribute("draggable", "true");
 
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
@@ -198,10 +199,53 @@ var taskStatusChangeHandler = function (event) {
     }
 };
 
+// function for dragging tasks to different columns
+var dragTaskHandler = function (event) {
+    var taskId = event.target.getAttribute("data-task-id");
+    event.dataTransfer.setData("text/plain", taskId);
+
+    var getId = event.dataTransfer.getData("text/plain");
+    console.log("getId:", getId, typeof getId);
+};
+
+var dropZoneDragHandler = function (event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        event.preventDefault();
+    }
+};
+
+var dropTaskHandler = function (event) {
+    var id = event.dataTransfer.getData("text/plain");
+    var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+    var dropZoneEl = event.target.closest(".task-list");
+    var statusType = dropZoneEl.id;
+    // set status of task based on dropZone id
+    var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+    if (statusType === "tasks-to-do") {
+        statusSelectEl.selectedIndex = 0;
+    }
+    else if (statusType === "tasks-in-progress") {
+        statusSelectEl.selectedIndex = 1;
+    }
+    else if (statusType === "tasks-completed") {
+        statusSelectEl.selectedIndex = 2;
+    }
+    dropZoneEl.appendChild(draggableElement);
+};
+
+
+
 // submit button event listener
 formEl.addEventListener("submit", taskFormHandler);
 // listening for any click in #page-content (sends it to taskButtonHandler to match for delete button)
 pageContentEl.addEventListener("click", taskButtonHandler);
 // listening for any change in taskStatusChangeHandler (drop down select menu/task status)
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+// listening for dragstart within #page-content or main html
+pageContentEl.addEventListener("dragstart", dragTaskHandler);
+// listening for dragover, calls dropZoneDragHandler function
+pageContentEl.addEventListener("dragover", dropZoneDragHandler);
+// listenging for drop event, calls dropTaskHandler
+pageContentEl.addEventListener("drop", dropTaskHandler);
 
