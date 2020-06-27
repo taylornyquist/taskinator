cfe1e84f87c2490e9b2d3ffd0fa8d3f3d4a7f372
Taylor Nyquist <tnyquist@gmail.com>
2020-06-26 23:41:14 -0500

    save tasks to arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var pageContentEl = document.querySelector("#page-content");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
+var tasks = [];
 var taskIdCounter = 0;
 
 var taskFormHandler = function (event) {
@@ -32,7 +33,8 @@ var taskFormHandler = function (event) {
     else {
         var taskDataObj = {
             name: taskNameInput,
-            type: taskTypeInput
+            type: taskTypeInput,
+            status: "to do"
         };
 
         createTaskEl(taskDataObj);
@@ -49,6 +51,14 @@ var completeEditTask = function (taskName, taskType, taskId) {
     taskSelected.querySelector("h3.task-name").textContent = taskName;
     taskSelected.querySelector("span.task-type").textContent = taskType;
 
+    // loop through tasks array and task object with new content
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+            tasks[i].name = taskName;
+            tasks[i].type = taskType;
+        }
+    };
+
     alert("Task Updated!");
 
     formEl.removeAttribute("data-task-id");
@@ -57,6 +67,9 @@ var completeEditTask = function (taskName, taskType, taskId) {
 
 // Create Task Function
 var createTaskEl = function (taskDataObj) {
+
+    console.log(taskDataObj);
+    console.log(taskDataObj.status);
 
     // create list item
     var listItemEl = document.createElement("li");
@@ -79,6 +92,11 @@ var createTaskEl = function (taskDataObj) {
 
     // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
+
+    // is this the right spot? Adding taskIdCounter to the object
+    taskDataObj.id = taskIdCounter;
+
+    tasks.push(taskDataObj);
 
     // increase task counter for next unique id
     taskIdCounter++;
@@ -156,6 +174,21 @@ var deleteTask = function (taskId) {
     taskSelected.remove();
 
     console.log(taskId);
+
+    // create new array to hold updated list of tasks
+    var updatedTaskArr = [];
+
+    // loop through current tasks
+    for (var i = 0; i < tasks.length; i++) {
+        // if tasks[i].id doesn't match the value of taskId, let's keep that task and push it into the new array
+        if (tasks[i].id !== parseInt(taskId)) {
+            updatedTaskArr.push(tasks[i]);
+        }
+    }
+
+    // reassign tasks array to be the same as updatedTaskArr
+    tasks = updatedTaskArr;
+
 };
 
 // function that edits the task
@@ -196,7 +229,17 @@ var taskStatusChangeHandler = function (event) {
     }
     else if (statusValue === "completed") {
         tasksCompletedEl.appendChild(taskSelected);
-    }
+    };
+
+    // update task's in tasks array
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+            tasks[i].status = statusValue;
+        }
+    };
+
+    console.log(tasks);
+
 };
 
 // function for dragging tasks to different columns
@@ -208,6 +251,7 @@ var dragTaskHandler = function (event) {
     console.log("getId:", getId, typeof getId);
 };
 
+// function defining drop zone
 var dropZoneDragHandler = function (event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
@@ -216,6 +260,7 @@ var dropZoneDragHandler = function (event) {
     }
 };
 
+// function handling the drop portion of the drag and drop
 var dropTaskHandler = function (event) {
     var id = event.dataTransfer.getData("text/plain");
     var draggableElement = document.querySelector("[data-task-id='" + id + "']");
@@ -235,6 +280,15 @@ var dropTaskHandler = function (event) {
 
     dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
+
+    // loop through tasks array to find and update the updated task's status
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(id)) {
+            tasks[i].status = statusSelectEl.value.toLowerCase();
+        }
+    }
+
+    console.log(tasks);
 
 };
 
